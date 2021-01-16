8cbfbf8a5cc19ab8052710027c66f65f2cfaf59a
atlanboa@gmail.com <tkd-gjs=12>
2021-01-16 17:39:58 +0900

    gnl

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghkim <atlanboa@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/15 04:28:01 by sanghkim          #+#    #+#             */
+/*   Updated: 2021/01/16 03:54:49 by sanghkim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <limits.h>
+# define ERROR -1
+# define SUCCESS 1
+# define SUCCESS_EOF 0
+
+int		get_next_line(int fd, char **line);
+char	*ft_strndup(char *str, size_t n);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *str);
+
+#endif
